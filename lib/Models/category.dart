684cd76182975b975669e2a633acbbb4e692855e
AwaisHaleem <awaishaleem20@gmail.com>
2022-08-07 12:52:33 +0500

    ccategory upload function added

--- lib/Models/category.dart
+++ lib/Models/category.dart
@@ -1,47 +1,59 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+FirebaseFirestore db = FirebaseFirestore.instance;
 
 class Category {
-  int id;
-  String name;
-  String type;
-  Category({required this.id, required this.name, required this.type});
+  String id;
+  String title;
+  String parentId;
+  Category({
+    required this.id,
+    required this.parentId,
+    required this.title,
+  });
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'id': id});
+    result.addAll({'title': title});
+    result.addAll({'parentId': parentId});
+
+    return result;
+  }
+
+  factory Category.fromMap(Map<String, dynamic> map) {
+    return Category(
+      id: map['id']?.toInt() ?? 0,
+      title: map['title'] ?? '',
+      parentId: map['parentId'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Category.fromJson(String source) =>
+      Category.fromMap(json.decode(source));
 }
 
-class Categories with ChangeNotifier{
-  List<Category> _categories = [
-    Category(
-      id: 1,
-      name: 'Ring',
-      type: 'Parent',
-    ),
-     Category(
-      id: 12,
-      name: 'Daimond',
-      type: 'Parent',
-    ),
-     Category(
-      id: 122,
-      name: 'Ring',
-      type: 'Parent',
-    ),
-     Category(
-      id: 21,
-      name: 'Gold',
-      type: 'Sub',
-    ),
-     Category(
-      id: 212,
-      name: 'Bengales',
-      type: 'Sub',
-    ),
-     Category(
-      id: 2212,
-      name: 'Neclas',
-      type: 'Sub',
-    ),
-  ];
+class Categories with ChangeNotifier {
+  final List<Category> _categories = [];
 
-  List<Category> get categories{
+  List<Category> get categories {
     return [..._categories];
+  }
+
+  uploadCatagory(String parentId, String title) async {
+    try {
+      await db
+          .collection("categories")
+          .add({"title": title, "parentId": parentId});
+    } catch (e) {
+      print('sufyan');
+    }
   }
 }
