6dd6e41a1628e7d205daa936d1366c78cc51b903
AwaisHaleem <awaishaleem20@gmail.com>
2022-08-08 12:20:14 +0500

    Merge branch 'master' of https://github.com/SufyanSajid/anees_casting

---
+++ lib/Screen/Common/splash.dart
@@ -0,0 +1,58 @@
+import 'package:anees_costing/Screen/Admin/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import '../../Models/auth.dart';
+import '../../contant.dart';
+import '../Auth/login.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  bool isFirst = true;
+
+  @override
+  void didChangeDependencies() async {
+    if (isFirst) {
+      isFirst = false;
+      Future.delayed(Duration(seconds: 1)).then((value) async {
+        bool isLogin = await Provider.of<Auth>(context, listen: false)
+            .tryAutoLogin()
+            .catchError((error) {
+          //  Navigator.of(context).pushReplacementNamed(LoginScreen.routeName);
+          print('error in autologin');
+        });
+        if (isLogin) {
+          Navigator.of(context).pushReplacementNamed(AdminHomePage.routeName);
+        } else {
+          Navigator.of(context).pushReplacementNamed(LoginScreen.routeName);
+        }
+      });
+    }
+
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Center(
+        child: Text(
+          'Anees Casting',
+          style: GoogleFonts.berkshireSwash(
+            fontSize: 60,
+            color: primaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
